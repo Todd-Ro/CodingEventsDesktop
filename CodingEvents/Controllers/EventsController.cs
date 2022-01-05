772246ca07a7762f335cc981325133d13ffad2f2
Todd Romkema <todd.romkema@gmail.com>
2022-01-05 08:04:28 -0600

    Add project files.

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CodingEvents.Controllers
+{
+    
+
+    public class EventsController : Controller
+    {
+        
+        static private List<string> eventList;
+
+
+        public IActionResult Index()
+        {
+            if(eventList is null)
+            {
+                eventList = new List<string>();
+            }
+            if(!eventList.Contains("REACT"))
+            {
+                eventList.AddRange(new List<string>
+                {
+                    "STL Coders",
+                    "Geek Gala",
+                    "REACT"
+                });
+            }
+            
+            ViewBag.alltheevs = eventList;
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string eventName)
+        {
+            if (eventList is null)
+            {
+                eventList = new List<string>();
+            }
+            eventList.Add(eventName);
+
+            return Redirect("/Events");
+        }
+    }
+
+    
+}
